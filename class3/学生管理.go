ea08374bd73d12e71c337fb4397648821bb53533
504233 <1149773887@qq.com>
2022-04-30 09:39:01 +0800

    Update 学生管理.go

--- class3/学生管理.go
+++ class3/学生管理.go
@@ -2,11 +2,13 @@ package main
 
 import "fmt"
 
+var studentlist []string
+
 func main() {
 	// 使用“面向对象”的思维方式编写一个学生信息管理系统。
 	//学生有id、姓名、年龄、分数等信息
 	//程序提供展示学生列表、添加学生、编辑学生信息、删除学生等功能
-	var studentlist []string
+	
 
 }
 
@@ -17,7 +19,7 @@ type student struct {
 	score int
 }
 
-func menu() {
+func menu(studentlist) {
 	fmt.Printf("欢迎使用学生管理系统\n")
 	fmt.Printf("=============================================\n")
 	fmt.Printf("请输入相应编号进入功能：\n")
@@ -33,13 +35,17 @@ func menu() {
 	case 0:
 		break
 	case 1:
-		addstudent()
+		addstudent(studentlist)
 	case 2:
-		editstudent()
+		editstudent(studentlist)
 	case 3:
-		delstudent()
+		delstudent(studentlist)
 	case 4:
-		showstudent()
+		showstudent(studentlist)
 	default:
 		fmt.Println("error!")
+}
+
+func addstudent(studentlist){
+	
 }
